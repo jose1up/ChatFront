0db61cad3753dc85f03d00b8cfe23e0283c5173d
Jose Miguel Alcaraz <90214208+jose1up@users.noreply.github.com>
2025-07-15 02:46:02 -0300

    cambio de url

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base : 'jose1up.github.io/ChatFront/'
+  base : 'https://jose1up.github.io/ChatFront/'
 })
